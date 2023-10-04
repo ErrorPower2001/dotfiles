3b4ef8c81360573aad47af221ab689eb0ba8e2ed
ErrorPower <ErrorPower2001@outlook.com>
2023-10-04 17:27:25 +0800

    - rio terminal config completed - rio terminal add 2 themes - wezterm config completed

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,20 +1,49 @@
--- Pull in the wezterm API
-local wezterm = require 'wezterm'
+-- 获取 Wezterm 接口
+local wezterm = require('wezterm')
 
--- This table will hold the configuration.
+-- 配置组
 local config = {}
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
+-- 对于较新版本的 Wezterm，使用 config_builder 将帮助获取简洁的错误信息
 if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
--- This is where you actually apply your config choices
+-- 在这里进行配置
 
--- For example, changing the color scheme:
-config.color_scheme = 'AdventureTime'
+-- 配置主题
+-- Source: https://github.com/mbadolato/iTerm2-Color-Schemes
+config.color_scheme = 'Afterglow'
+-- Source: https://github.com/sonph/onehalf
+-- config.color_scheme = 'OneHalfDark'
 
--- and finally, return the configuration to wezterm
+-- Shell
+config.default_prog = { 'pwsh', '-Login' }
+
+-- Font
+config.font = wezterm.font('Maple Mono SC NF')
+
+-- 行列宽
+config.initial_cols = 120
+config.initial_rows = 30
+
+-- Wezterm Event
+-- GUI start event
+-- 当 wezterm 以界面启动时调用
+wezterm.on( 'gui-startup',
+	function(cmd)
+		-- Wezterm window position configuration
+		-- 获取对象
+		local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
+		-- 设置窗口启动位置
+		window:gui_window():set_position(20, 60)
+		window:gui_window():set_position(20, 20)
+		---window:gui_window():set_inner_size(1280, 720)
+	end
+)
+
+-- 标题栏设置
+config.window_decorations = "RESIZE"
+
+-- 最后，应用你的配置
 return config
-
