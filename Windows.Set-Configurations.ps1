72485a96b9e67059fa9d8f6be1d67da8c383daff
ErrorPower <ErrorPower2001@outlook.com>
2023-10-23 21:38:09 +0800

    PowerShell 部署脚本

--- Windows.Set-Configurations.ps1
+++ Windows.Set-Configurations.ps1
@@ -1,22 +1,69 @@
-$Configuration2Path = @(
-	(
-		'.\bash\.bash_profile',
-		"$HOME\"
-	),
-	(
-		'.\bash\.bashrc',
-		"$HOME\"
-	),
-	(
-		'.\bash\ep.bashrc',
-		"$HOME\"
-	),
-	(
-		'.\nvim\',
-		"$($Env:LOCALAPPDATA)\"
-	),
-	(
-		'.\rio\',
-		"$($Env:LOCALAPPDATA)\"
+###############################################################################
+# Create symbolic link to config my dotfiles
+###############################################################################
+
+
+###############################################################################
+# Handle Windows's $HOME\.config
+###############################################################################
+if ( $IsWindows ) {
+	if ( (Test-Path -Path "$HOME\.config\") -and !(Get-Item -Path "$HOME\.config\" | Select-Object -ExpandProperty LinkType) ) {
+		Get-ChildItem -Path "$HOME\.config\" | `
+			Move-Item -Force -Destination "$HOME\AppData\Local\"
+		
+		Remove-Item -Force -Recurse -Path "$HOME\.config\"
+		
+		New-Item -Force -ItemType SymbolicLink `
+		-Path "$HOME\.config" `
+		-Target "$HOME\AppData\Local\"
+	}
+	
+	New-Item -Force -ItemType SymbolicLink `
+	-Path "$HOME\.config" `
+	-Target "$HOME\AppData\Local\"
+}
+
+
+###############################################################################
+# Function
+###############################################################################
+# 配置文件目录 $HOME\.config
+# 该方法仅对 New-Item -ItemType SymbolicLink 进行包装
+function Set-ConfigurationSymbolicLink {
+	param(
+		[string]$Config,
+		[string]$Name
 	)
-)
+
+	$ConfigInRoot = "$PSScriptRoot\$Config"
+	
+	# 如果未填写名称
+	if ( ! $Name ) {
+		# 名称设置为配置文件名称
+		$Name = (Get-Item -Path $ConfigInRoot).Name
+	}
+	
+	New-Item -Force -ItemType SymbolicLink `
+	-Path "$HOME\.config\$Name" `
+	-Target "$( (Get-Item -Path $ConfigInRoot).FullName )"
+}
+
+
+###############################################################################
+# Configuration
+###############################################################################
+Set-ConfigurationSymbolicLink -Config alacritty
+Set-ConfigurationSymbolicLink -Config hypr
+Set-ConfigurationSymbolicLink -Config rio
+Set-ConfigurationSymbolicLink -Config waybar
+Set-ConfigurationSymbolicLink -Config wezterm
+Set-ConfigurationSymbolicLink -Config xdg-user-dirs
+Set-ConfigurationSymbolicLink -Config neoORvim -Name nvim
+
+Set-ConfigurationSymbolicLink -Config neoORvim -Name "..\vimfiles"
+
+
+###############################################################################
+# Unset funcrion
+###############################################################################
+Remove-Item -Path Function:\Set-ConfigurationSymbolicLink
