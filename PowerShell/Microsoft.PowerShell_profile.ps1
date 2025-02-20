af16d65d53386906c9ebc89563d94d6a0f35c1b9
ErrorPower2001 <ErrorPower2001@outlook.com>
2025-02-21 02:27:10 +0800

    feat: posh-git 不再自动加载，需求环境变量 USE_POSHGIT

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,7 @@
 function Write-HostWithTime {
 	param([String]$Object, [switch]$NoNewline)
 	Write-Host -NoNewline -Object "`e[92m[$(Get-Date -Format "MM:hh:ss.ffff")]`e[0m"
-	if($NoNewline) {
+	if ($NoNewline) {
 		Write-Host -NoNewline -Object $Object
 	}
 	else {
@@ -10,6 +10,10 @@ function Write-HostWithTime {
 }
 
 Write-HostWithTime -Object "Start loading `$PROFILE.CurrentUserCurrentHost"
+
+
+Write-HostWithTime -Object "Dot sourcing more commands in `$HOME\.config\pwsh\"
+Get-ChildItem -Path $HOME\.config\pwsh\ | %{ . $_.FullName }
 
 
 Write-HostWithTime -Object "Setting input and output encoding to UTF-8"
@@ -23,9 +27,7 @@ Write-HostWithTime -Object "Importing scoop-completion module from scoop bucket:
 Import-Module "$((Get-Item (Get-Command scoop.ps1).Path).Directory.Parent.FullName)\modules\scoop-completion"
 
 
-Write-HostWithTime -Object "Importing posh-git module"
-Import-Module "$((Get-Module -ListAvailable -Name posh-git).Path)"
-
-
-Write-HostWithTime -Object "Dot sourcing more commands in `$HOME\.config\pwsh\"
-Get-ChildItem -Path $HOME\.config\pwsh\ | %{ . $_.FullName }
+if ($env:USE_POSHGIT -eq $true) {
+	Write-HostWithTime -Object "Importing posh-git module"
+	Import-Module "$((Get-Module -ListAvailable -Name posh-git).Path)"
+}
