6813011fd80b15e47af76e9a06feb7a251f0f9af
ErrorPower2001 <ErrorPower2001@outlook.com>
2025-03-02 01:12:38 +0800

    fix: Prompt's function scope

--- pwsh/Prompt.ps1
+++ pwsh/Prompt.ps1
@@ -1,16 +1,15 @@
-function trim_char ($color, $char, [switch]$reverse) {
-	$output = ""
-	$color -= 10
-	if ($reverse) {
-		$output = "`e[7m"
-	}
-	$output += "`e[${color}m"
-	$output += $char
-	$output += "`e[0m"
-	return $output
-}
-
 function prompt {
+	function trim_char ($color, $char, [switch]$reverse) {
+		$output = ""
+		$color -= 10
+		if ($reverse) {
+			$output = "`e[7m"
+		}
+		$output += "`e[${color}m"
+		$output += $char
+		$output += "`e[0m"
+		return $output
+	}
 	$prompt = ""
 
 	$character = "`u{e0b0}"
@@ -44,4 +43,3 @@ function prompt {
 }
 
 Set-PSReadLineOption -PromptText "> ", "! "
-Remove-Item -Path $Function:trim_char
