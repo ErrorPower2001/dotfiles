0227ea4c2bc74a716ad3341498a46867ac7cbcc9
ErrorPower2001 <ErrorPower2001@outlook.com>
2025-03-02 01:08:04 +0800

    feat: New pwsh prompt settings

---
+++ pwsh/Prompt.ps1
@@ -0,0 +1,47 @@
+function trim_char ($color, $char, [switch]$reverse) {
+	$output = ""
+	$color -= 10
+	if ($reverse) {
+		$output = "`e[7m"
+	}
+	$output += "`e[${color}m"
+	$output += $char
+	$output += "`e[0m"
+	return $output
+}
+
+function prompt {
+	$prompt = ""
+
+	$character = "`u{e0b0}"
+
+	$prompt += (trim_char -color 44 -char $character -reverse)
+	$prompt += "`e[37;44m"
+	$prompt += " "
+	$prompt += "pwsh"
+	$prompt += " "
+	$prompt += "`e[0m"
+	$prompt += (trim_char -color 44 -char $character)
+
+
+	#...$prompt += " "
+
+
+	$prompt += (trim_char -color 49 -char $character -reverse)
+	$prompt += "`e[7m"
+	$prompt += " "
+	$prompt += "$($ExecutionContext.SessionState.Path.CurrentLocation)"
+	$prompt += " "
+	$prompt += "`e[27m"
+	$prompt += (trim_char -color 49 -char $character)
+
+
+	$prompt += "`n"
+	$prompt += "$('>' * ($NestedPromptLevel + 1))"
+	$prompt += " "
+
+	return $prompt
+}
+
+Set-PSReadLineOption -PromptText "> ", "! "
+Remove-Item -Path $Function:trim_char
