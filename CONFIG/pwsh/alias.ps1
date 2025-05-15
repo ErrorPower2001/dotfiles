2ad1eed03a4a1cf5d109103c9d44da01f384baba
ErrorPower2001 <ErrorPower2001@outlook.com>
2025-05-15 11:00:17 +0800

    feat: pwsh alias mpv new option

--- CONFIG/pwsh/alias.ps1
+++ CONFIG/pwsh/alias.ps1
@@ -1,2 +1,2 @@
-Function mpv { mpv.exe --screenshot-format=png $args }
+Function mpv { mpv.exe --screenshot-format=png --keep-open=yes $args }
 #...Set-Alias -Name mpv -Value "mpv --screenshot-format=png"
