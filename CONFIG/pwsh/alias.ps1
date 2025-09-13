6b83844ee69ddc8ee400aa75cfcc22e2a4a7f717
ErrorPower2001 <ErrorPower2001@outlook.com>
2025-09-14 02:16:10 +0800

    feat: add mpvimage, mvi, code

--- CONFIG/pwsh/alias.ps1
+++ CONFIG/pwsh/alias.ps1
@@ -2,4 +2,14 @@ if($IsWindows) {
 	Function mpv {
 		mpv.com --screenshot-format=png --keep-open=yes $args
 	}
+	Function mvi {
+		mpv.com --config-dir="$HOME\Source\repos\mpv-image-viewer\" $args
+	}
+	Function mpvimage {
+		mvi $args
+	}
+
+	Function code {
+		code.cmd --disable-workspace-trust $args
+	}
 }
