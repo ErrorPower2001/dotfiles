25f7d05fdf4f440e42cc21873db128f09e7531c1
ErrorPower2001 <ErrorPower2001@outlook.com>
2025-09-20 17:52:48 +0800

    feat: rm alias to uutils

--- CONFIG/pwsh/alias.ps1
+++ CONFIG/pwsh/alias.ps1
@@ -1,15 +1,23 @@
 if($IsWindows) {
 	Function mpv {
-		mpv.com --screenshot-format=png --keep-open=yes $args
+		mpv.com --screenshot-format=png --keep-open=yes $args;
 	}
 	Function mvi {
-		mpv.com --config-dir="$HOME\Source\repos\mpv-image-viewer\" $args
+		mpv.com --config-dir="$HOME\Source\repos\mpv-image-viewer\" $args;
 	}
 	Function mpvimage {
-		mvi $args
+		mvi $args;
 	}
 
 	Function code {
-		code.cmd --disable-workspace-trust $args
+		code.cmd --disable-workspace-trust $args;
+	}
+
+	if ( Test-Path -Path "$HOME\scoop\apps\uutils-coreutils-lean\current\coreutils.exe" ) {
+		Remove-Alias -Name rm;
+		Function rm {
+			& "$HOME\scoop\apps\uutils-coreutils-lean\current\coreutils.exe" `
+				rm $args;
+		}
 	}
 }
